3a23668a6c9399feaedb1b1543112c476703390b
zaqc <teamcoders@gmail.com>
2018-08-08 21:13:57 +0300

    sd_reader rc

--- main.v
+++ main.v
@@ -1,13 +1,83 @@
 module main;
 
-	function [15:0] nextCRC16_D8;
+	reg		[0:0]		clk;
+	initial clk = 0;
+	always #10 clk = ~clk;
+	
+	reg		[0:0]		rst_n;
+	initial rst_n = 0;
 
+	initial begin
+		rst_n = 1'd0;
+		$dumpfile("dumpfile.vcd"); 
+		$dumpvars(0);
+		#20
+		rst_n = 1'd1;
+	end
+	
+	initial begin
+		$display("start");
+		#1000
+		$finish();
+	end
+	
+	reg		[15:0]		cntr;
+	initial cntr = 16'd0;
+	always @ (posedge clk)
+		cntr <= cntr + 1'd1;
+		
+	reg		[15:0]		crc;
+	initial crc = 0;
+	
+	reg		[7:0]		crc_in;
+	initial crc_in = 8'd1;
+	
+	always @ (posedge clk)
+		crc_in <= crc_in + 1'd1;
+
+/*	
+	always @ (posedge clk)
+		crc <= calc_crc(crc_in, crc);
+		
+	always @ (posedge clk)
+		$display("0x%04X", crc);
+*/	
+
+	reg		[0:0]		sw;
+	reg		[3:0]		idat;
+	integer i;
+	reg		[0:0]		run_cntr;
+	
+	initial begin
+		sw = 1'd0;
+		run_cntr = 1'd0;
+		idat = 4'hF;
+		#30
+		sw = 1'd1;
+		#20
+		sw = 1'd0;
+		#20
+		run_cntr = 1'd1;
+	end
+	
+	always @(posedge clk) 
+		if(run_cntr) idat <= idat + 1'd1;
+	
+	sd_reader sd_reader_unit(
+		.rst_n(rst_n),
+		.clk(clk),
+				
+		.i_start_writing(sw),
+		.i_sd_data(idat),
+		.i_buf_len(10'd8)
+	);
+
+	function [15:0] nextCRC16_D8;
 		input [7:0] Data;
 		input [15:0] crc;
 		reg [7:0] d;
 		reg [15:0] c;
 		reg [15:0] newcrc;
-		
 		begin
 			d = Data;
 			c = crc;
@@ -31,41 +101,7 @@ module main;
 			nextCRC16_D8 = newcrc;
 		end
 	endfunction
-
-
-	reg		[0:0]		clk;
-	initial clk = 0;
-	always #10 clk = ~clk;
-
-	initial begin
-		$dumpfile("dumpfile.vcd"); 
-		$dumpvars(0);
-	end
 	
-	initial begin
-		$display("start");
-		#300
-		$finish();
-	end
-	
-	reg		[15:0]		cntr;
-	initial cntr = 16'd0;
-	always @ (posedge clk)
-		cntr <= cntr + 1'd1;
-		
-	reg		[15:0]		crc;
-	initial crc = 0;
-	
-	reg		[7:0]		crc_in;
-	initial crc_in = 8'd1;
-	
-	always @ (posedge clk)
-		crc_in <= crc_in + 1'd1;
-	
-	always @ (posedge clk)
-		crc <= nextCRC16_D8(crc_in, crc);
-		
-	always @ (posedge clk)
-		$display("0x%04X", crc);
+	initial $display("CRC16 = 0x%04X", nextCRC16_D8(8'd1, 16'd0));
 
 endmodule
